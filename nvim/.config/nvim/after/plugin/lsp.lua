572de0bf4bbe711a38899bb911e0c97fe2d5e7e1
Laurens De la Marche <laurens.dlm@gmail.com>
2023-11-11 15:20:43 +0100

    Basic setup snippets neovim

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,4 +1,6 @@
 local lsp_zero = require('lsp-zero')
+local cmp = require('cmp')
+local cmp_format = require('lsp-zero').cmp_format()
 
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
@@ -29,3 +31,18 @@ require('lspconfig').lua_ls.setup({
     }
   }
 })
+
+require("luasnip.loaders.from_vscode").lazy_load({
+  paths = { "~/Library/Application Support/Code/User/snippets/" } })
+
+cmp.setup({
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }
+  },
+  --- (Optional) Show source name in completion menu
+  formatting = cmp_format,
+})
+
+vim.api.nvim_set_keymap('i', '<Tab>', "luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'",
+  { expr = true, silent = true })
--- nvim/.config/nvim/lua/vanilla/lazy.lua
+++ nvim/.config/nvim/lua/vanilla/lazy.lua
@@ -90,6 +90,7 @@ require("lazy").setup({
       { 'hrsh7th/nvim-cmp' },
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'L3MON4D3/LuaSnip' },
+      { "saadparwaiz1/cmp_luasnip" }
     }
   },
   -- Devicons
